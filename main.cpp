b913aba28585b7817409fb4147ac694237be20a2
Emanuel Oly <olysancheznicolas@gmail.com>
2024-06-05 17:37:06 -0400

    Verificando si el Mes o el Dia estan dentro de los limites

--- main.cpp
+++ main.cpp
@@ -6,21 +6,37 @@ int main() {
     map <string,string> database;
     string linea1;
     string date,evento,command;
-
+    int year,month,day;
+    char s1,s2;
     while(getline(cin,linea1)){ 
     
     istringstream s(linea1);
     s>> command>> date >> evento;
     
+    istringstream d(date);
+    d >> year >> s1 >> month >> s2 >> day;
+   
+
     if(command=="Add"){
-        database[date]=evento;
+        if(month<1 || month >12){
+            cout<<"Mes inválido: "<<month<<endl;
+            }
+        if(day<1 || day>31){
+            cout<<"Día inválido: "<<day<<endl;
+            }
+        else{ 
+            database[date]=evento;
+        }
     }
     if(command=="Print"){
         for(auto x:database){
-        cout<<x.first<<" "<<x.second<<endl;
+            
+            cout<<x.first<<" "<<x.second<<endl;
+            
         }
     }
 
 }
 return 0;
+
 }
