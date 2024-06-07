c6cef7817093698c6ee865487753e8ba78e1b548
Emanuel Oly <olysancheznicolas@gmail.com>
2024-06-07 12:47:39 -0400

    Rehaciendo el map, agregando un set al map para que un indice tenga varios valores

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <sstream>
 #include <map>
+#include <string>
+#include <set>
 using namespace std;
 int main() {
-    map <string,string> database;
+    map <string,set<string>> database;
     string linea1;
     string date,evento,command;
     int year,month,day;
@@ -25,18 +27,37 @@ int main() {
             cout<<"Día inválido: "<<day<<endl;
             }
         else{ 
-            database[date]=evento;
-        }
-    }
-    if(command=="Print"){
-        for(auto x:database){
-            
-            cout<<x.first<<" "<<x.second<<endl;
-            
+            database[date].insert(evento);
         }
     }
 
+    if(command=="Print"){
+        for(auto x:database){
+            for(auto event:x.second)
+            
+            cout<<x.first<<" "<<evento<<endl;
+            
+        }
+    }
+    /* 
+    if(command=="Del"){
+        auto it= database.find(date,evento);
+        if(it!= database.end()){
+            auto xd= it->second;
+            auto xdd= xd.erase();
+            database.emplace(date,xdd);
+                if(it->second.empty()){
+                    database.erase(it);
+                }
+            cout<<"Deleted Sucessfully"<<endl;
+        }
+        else{
+            cout<<"Event not Found"<<endl;
+        }
+    }
+*/
 }
+
 return 0;
 
 }
